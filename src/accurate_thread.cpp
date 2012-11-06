54b55e2acd8eaf53e8d4fc2b35d8ff62a9f0f02b
Mateusz Bysiek <mb@mbdev.pl>
2012-11-06 20:07:47 +0100

    connected accurate algorithm with gui, added public method removed_first() to domino_problem
    note: x&y coordinates are in reversed order in event emmiter
      (i.e. y is horizontal and x is vertical axis), the problem
      is solved by reversing order of the arguments
    
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- src/accurate_thread.cpp
+++ src/accurate_thread.cpp
@@ -2,25 +2,24 @@
 
 accurate_thread::accurate_thread(domino_problem_input &input) : input(nullptr)
 {
-    this->input = &input;
+   this->input = &input;
 }
 
 void accurate_thread::run()
 {
-    domino_problem prob(*input);
-    domino_problem_solver solver(prob);
+   domino_problem prob(*input);
+   domino_problem_solver solver(prob);
 
-    solver.execute(false, 4000, false, DEPTH_SORT, true);
+   solver.execute(false, 4000, false, DEPTH_SORT, true);
 
-    domino_problem::solution_t frst = solver.find_first_best_solution();
-  //  domino_problem best_state = frst.last().value();
-
-    for(domino_problem::solution_t::elem i = frst.first(); i; ++i)
-        std::cout << (*i).board_str() << std::endl << std::endl;
-
-    domino_problem::solution_t::elem i = frst.last();
-     std::cout << (*i).removed_str() << std::endl
-           << (*i).board_str() << std::endl << std::endl;
+   domino_problem::solution_t frst = solver.find_first_best_solution();
+   domino_problem& best_state = frst.last().value_ref();
+   for(domino_problem::elements_t::elem_const e = best_state.removed_first(); e; ++e)
+   {
+      size_t x = (*e)->x;
+      size_t y = (*e)->y;
+      emit threadRemovePiece((int)y,(int)x);
+   }
 
 }
 
